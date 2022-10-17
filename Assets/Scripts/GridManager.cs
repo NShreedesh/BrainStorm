f9d98ec3b5d0b8ad458cc13d8288829f19438c5e
NShreedesh <65639214+NShreedesh@users.noreply.github.com>
2022-10-17 07:49:18 +0545

    add left and right movement while swiping tile

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -1,44 +1,78 @@
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using static UnityEngine.InputSystem.InputAction;
 
 public class GridManager : MonoBehaviour
 {
-    [SerializeField]
-    private Tilemap arrowTileMap;
+    public Camera cam;
 
-    [SerializeField]
-    private Tile arrowTilePrefab;
+    public Tilemap arrowTileMap;
+    public Tile arrowTilePrefab;
+    public Tile selectedTile;
+    public LayerMask tileLayerMask;
 
-    [SerializeField]
-    private Tile spawnedTile;
+    public Input input;
+    public Vector2 startPosition;
+    public Vector2 endPosition;
+
+    public float dragDistance;
+
+    private void OnEnable()
+    {
+        input = new Input();
+        input.Enable();
+
+        input.Player.Click.started += OnClickStarted;
+        input.Player.Click.canceled += OnClickCanceled;
+
+        input.Player.Position.performed += OnClickPerformed;
+    }
 
     private void Start()
     {
         var cellPosition = arrowTileMap.WorldToCell(transform.position);
-        spawnedTile = Instantiate(arrowTilePrefab, cellPosition + new Vector3(0.5f, 0.5f), Quaternion.identity);
+        Instantiate(arrowTilePrefab, cellPosition + new Vector3(0.5f, 0.5f), Quaternion.identity);
     }
 
-    private void Update()
+    private void OnClickStarted(CallbackContext ctx)
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            var cellPosition = arrowTileMap.WorldToCell(spawnedTile.transform.position + new Vector3(-1, 0));
-            if (!arrowTileMap.HasTile(cellPosition))
-            {
-                return;
-            }
+        var worldMousePosition = cam.ScreenToWorldPoint(input.Player.Position.ReadValue<Vector2>());
+        RaycastHit2D raycastHit = Physics2D.Raycast(worldMousePosition, Vector2.zero, 10, tileLayerMask);
+        if (raycastHit.collider == null) return;
 
-            spawnedTile.Move(cellPosition);
+        raycastHit.collider.TryGetComponent<Tile>(out Tile tile);
+        if (tile == null) return;
+
+        selectedTile = tile;
+        startPosition = cam.ScreenToWorldPoint(input.Player.Position.ReadValue<Vector2>());
+    }
+
+    private void OnClickCanceled(CallbackContext obj)
+    {
+        selectedTile = null;
+    }
+
+    private void OnClickPerformed(CallbackContext ctx)
+    {
+        if (!input.Player.Click.IsPressed()) return;
+        if (selectedTile == null) return;
+
+        endPosition = cam.ScreenToWorldPoint(input.Player.Position.ReadValue<Vector2>());
+        var draggedDistance = endPosition - startPosition;
+
+        if (draggedDistance.x < -dragDistance)
+        {
+            var cellPosition = arrowTileMap.WorldToCell(selectedTile.transform.position + new Vector3(-1, 0));
+            if (!arrowTileMap.HasTile(cellPosition)) return;
+            selectedTile.Move(cellPosition);
+            startPosition = endPosition;
         }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        else if (draggedDistance.x > dragDistance)
         {
-            var cellPosition = arrowTileMap.WorldToCell(spawnedTile.transform.position + new Vector3(1, 0));
-            if (!arrowTileMap.HasTile(cellPosition))
-            {
-                return;
-            }
-
-            spawnedTile.Move(cellPosition);
+            var cellPosition = arrowTileMap.WorldToCell(selectedTile.transform.position + new Vector3(1, 0));
+            if (!arrowTileMap.HasTile(cellPosition)) return;
+            selectedTile.Move(cellPosition);
+            startPosition = endPosition;
         }
     }
 }
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,4 +1,6 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Tile : MonoBehaviour
 {
